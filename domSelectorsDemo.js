88090ac49b0e31f62b1cd91c515a04a4367ad512
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-09-02 15:05:37 -0400

    Initial commit

---
+++ domSelectorsDemo.js
@@ -0,0 +1,17 @@
+
+var liHighlighted = document.getElementById("highlight");
+var liBolded = document.getElementsByClassName("bolded");
+var liTags = document.getElementsByTagName("li");
+
+console.dir(liHighlighted);
+
+var highlightedTag = document.querySelector("#highlight");
+
+var boldedTag = document.querySelector(".bolded");
+
+var h1Tag = document.querySelector("h1");
+
+var allLis = document.querySelectorAll("li");
+
+
+var allH1s = document.querySelectorAll("h1");
